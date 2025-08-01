99dc3c427ce8b2adb3b4dcc83aea6929317aae99
Andrei Adlawan <97928373+Andraannn@users.noreply.github.com>
2025-08-01 10:26:49 +0800

    fixed errors

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../assets/css/about.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
